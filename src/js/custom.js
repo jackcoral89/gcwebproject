fc707e5a92b2656617a199013547c5caf7a82884
Giacomo Corallini <jackcoral89@4zur3.local>
2018-08-06 14:17:07 +0200

    finalizzato bottone custom per la gestione dell'audio di background

---
+++ src/js/custom.js
@@ -0,0 +1,21 @@
+const audio = document.getElementById('bg_audio');
+const changeCssClass = document.getElementById('strips-control');
+audio.volume = 1;
+document.getElementById('strips-control')
+    .addEventListener('click', function changeAudio() {
+        
+        if (audio.volume) {
+            $('#bg_audio').animate({
+                volume: 0
+            }, 2000);
+            changeCssClass.className += ' rest_audio';
+        } else {
+            $('#bg_audio').animate({
+                volume: 1
+            }, 2000);
+            if ($('#strips-control').hasClass('rest_audio')) {
+                $('#strips-control').addClass('up_audio');
+            }
+        }
+
+    });
